213339bbc7c0269895b6f7a839f0014c3f1a1464
Leandro Azevedo <leandro.s.azevedo@gmail.com>
2022-10-01 15:15:14 +0200

    Include new source for the data

---
+++ src/main/java/GetVesselDb.java
@@ -0,0 +1,7 @@
+public class GetVesselDb implements GetVesselsInterface{
+
+    @Override
+    public void ReadVessels() {
+        System.out.println("I'll receive the data from a database");
+    }
+}
---
+++ src/main/java/GetVesselsFile.java
@@ -0,0 +1,7 @@
+public class GetVesselsFile implements GetVesselsInterface{
+
+    @Override
+    public void ReadVessels() {
+        System.out.println("I'll read the vessel list from a file");
+    }
+}
